65521891e8cabafdd626d4bf6fb03211c1677975
Nick Kasprak <nkasprak@gmail.com>
2023-01-04 11:03:57 -0500

    postinstall maybe works now

--- topo.js
+++ topo.js
@@ -1,6 +1,10 @@
 var fs = require("fs");
 var states = JSON.parse(fs.readFileSync("geojson/ne_50m_admin_1_states_provinces_lakes.json", "utf-8"));
 var puerto_rico = JSON.parse(fs.readFileSync("geojson/cb_2019_72_sldu_500k.json", "utf-8"))
+var cwd = process.cwd();
+if (process.env.INIT_CWD) {
+  cwd = process.env.INIT_CWD;
+}
 states.features = states.features.filter(function(a) {
   return a.properties.admin==="United States of America";
 });
@@ -16,7 +20,7 @@ states.features.push(pr_unified);
 var topology = topojson.topology({states: states});
 topology = topojson.quantize(topology, 1e4);
 try {
-  fs.mkdirSync("./html/geography")
+  fs.mkdirSync(cwd + "/html/geography")
 } catch (ex) {}
-fs.writeFileSync("./html/geography/states_50m_topo.json", JSON.stringify(topology), "utf-8");
+fs.writeFileSync(cwd + "/html/geography/states_50m_topo.json", JSON.stringify(topology), "utf-8");
 console.log("Wrote topo file at html/geography/states_50m_topo.json");
